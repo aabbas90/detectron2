6aca2d2b3b8369753b542eefed4fc38c4035af10
Ahmed Abbas <ahmed.a.mirza1@gmail.com>
2021-01-05 19:13:23 +0100

    changed eval batch size

--- detectron2/data/build.py
+++ detectron2/data/build.py
@@ -390,7 +390,7 @@ def build_detection_test_loader(cfg, dataset_name, mapper=None):
     sampler = InferenceSampler(len(dataset))
     # Always use 1 image per worker during inference since this is the
     # standard when reporting inference time in papers.
-    batch_sampler = torch.utils.data.sampler.BatchSampler(sampler, 1, drop_last=False)
+    batch_sampler = torch.utils.data.sampler.BatchSampler(sampler, 8, drop_last=False) #TODOAA
 
     data_loader = torch.utils.data.DataLoader(
         dataset,
